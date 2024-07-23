21a8c9c2d8b8940a44df3db833d4c6233c55e75c
OluwaTobba <142856021+OluwaTobba@users.noreply.github.com>
2024-07-23 13:37:15 +0100

    final commit

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -5,13 +5,13 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.EMRS_FIREBASE_API_KEY,
-  authDomain: process.env.EMRS_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.EMRS_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.EMRS_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.EMRS_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.EMRS_FIREBASE_APP_ID,
-  measurementId: process.env.EMRS_FIREBASE_MEASUREMENT_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
